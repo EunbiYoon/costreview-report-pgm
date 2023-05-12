2e8e55088f692ddbe5f4b1009e6d1a2b5694aaf9
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-05-12 07:59:51 -0500

    first commit

---
+++ json_graph.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import openpyxl 
+
+read_excel=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/BPAPAC_0512.xlsx", sheet_name="FL_BPA")
+extract_data=read_excel.drop(["Model"],axis=1)
+extract_data.index=extract_data["Tool"]
+extract_data=extract_data.drop(["Tool"],axis=1)
+print(extract_data)
+
+#column_list
+column_list=list(extract_data.columns)
+
+# column json file format
+column_json=str({"columns":column_list}).replace("{",'').replace("}",'').replace("'",'"').replace('nan','"nan"')
+print("")
+print(column_json+","+"")
+
+
+#index_list
+for i in range(len(extract_data.index)):
+    if i==len(extract_data.index)-1:
+        aa='"'+str(extract_data.index[i])+'":'+str(extract_data.iloc[i].tolist())
+    else:
+        aa='"'+str(extract_data.index[i])+'":'+str(extract_data.iloc[i].tolist())+","
+    print(aa)
+
+# value1=list(read_excel["vs BOM"].round(1))
+# value2=list(read_excel["PO Price Change"].round(1))
+# value3=list(read_excel["Substitute Change"].round(1))
+# value4=list(read_excel["PO + Substitute"].round(1))
+
+
+# data,index json file format
+# data_json=str({"vs BOM":value1,
+#                "PO Price Change":value2, 
+#                "Substitute Change":value3,
+#                "PO + Substitute":value4}).replace("{",'').replace("}",'').replace("'",'"').replace('nan','"nan"')
+# print(","+data_json)
+
+
+
