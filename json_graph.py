527f710e562658f8c2060a569028c5d53e51ca0c
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-05-15 12:57:09 -0500

    werwer

--- json_graph.py
+++ json_graph.py
@@ -15,19 +15,34 @@ column_json=str({"columns":column_list}).replace("{",'').replace("}",'').replace
 print("")
 print(column_json+","+"")
 
-
 #index_list
-for i in range(len(extract_data.index)):
-    if i==len(extract_data.index)-1:
-        aa='"'+str(extract_data.index[i])+'":'+str(extract_data.iloc[i].tolist())
-    else:
-        aa='"'+str(extract_data.index[i])+'":'+str(extract_data.iloc[i].tolist())+","
-    print(aa)
+extract_data=extract_data.T
+value1=list(extract_data["F3U8CNU3W.ABWEUUS"].round(1))
+value2=list(extract_data["F3P2CYUAW.ABWEUUS"].round(1))
+value3=list(extract_data["F3P2CYUAT.ASSEUUS"].round(1))
+value4=list(extract_data["F3P2CYUBW.ABWEUUS"].round(1))
+value5=list(extract_data["F3P2CYUBE.ABLEUUS"].round(1))
 
-# value1=list(read_excel["vs BOM"].round(1))
-# value2=list(read_excel["PO Price Change"].round(1))
-# value3=list(read_excel["Substitute Change"].round(1))
-# value4=list(read_excel["PO + Substitute"].round(1))
+# data,index json file format
+data_json=str({"F3U8CNU3W.ABWEUUS":value1,
+               "F3P2CYUAW.ABWEUUS":value2, 
+               "F3P2CYUAT.ASSEUUS":value3,
+               "F3P2CYUBW.ABWEUUS":value4,
+               "F3P2CYUBE.ABLEUUS":value5}).replace("{",'').replace("}",'').replace("'",'"').replace('nan','"nan"')
+print(","+data_json)
+
+# #index_list
+# for i in range(len(extract_data.index)):
+#     if i==len(extract_data.index)-1:
+#         aa='"'+str(extract_data.index[i])+'":'+str(extract_data.iloc[i].tolist())
+#     else:
+#         aa='"'+str(extract_data.index[i])+'":'+str(extract_data.iloc[i].tolist())+","
+#     print(aa)
+
+# # value1=list(read_excel["vs BOM"].round(1))
+# # value2=list(read_excel["PO Price Change"].round(1))
+# # value3=list(read_excel["Substitute Change"].round(1))
+# # value4=list(read_excel["PO + Substitute"].round(1))
 
 
 # data,index json file format
---
+++ json_table_item.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import openpyxl 
+
+read_excel=pd.read_excel("C:/Users/RnD Workstation/Documents/CostReview/0512/BPAPAC_0512.xlsx", sheet_name="FL_PAC_I")
+
+########### increase_item
+#column_list
+column_list=list(read_excel.columns)
+
+# column json file format
+column_json=str({"columns":column_list}).replace("{",'').replace("}",'').replace("'",'"').replace('nan','""').replace("blank","").replace(".1","")
+print("")
+print(column_json+",")
+
+#row_list
+read_excel=read_excel.T
+read_excel.reset_index(inplace=True, drop=True)
+read_excel=read_excel.T
+
+#row_list - index
+bb=pd.DataFrame()
+for i in range(len(read_excel.index)):
+    bb.at[0,i]='"index":"'+str(read_excel.at[i,0])+'",'
+
+print('"rows":[')
+#row_list - values
+for i in range(len(read_excel.index)-1):
+    if i==len(read_excel.index)-2:
+        aa=str(list(read_excel.iloc[i][1:])).replace("'",'"').replace('nan','""')
+        aA='"values":'+aa
+        AA="{"+str(bb.at[0,i])+aA+"}"
+        print(AA)
+    else:
+        aa=str(list(read_excel.iloc[i][1:])).replace("'",'"').replace('nan','""')
+        aA='"values":'+aa
+        AA="{"+str(bb.at[0,i])+aA+"},"
+        print(AA)
+print(']')
